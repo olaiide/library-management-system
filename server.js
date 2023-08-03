5db89941ae58a125d64377031d70106890b96336
Olaide Kolade <horlaide909@gmail.com>
2023-08-03 21:04:29 +0100

    chore : set up server and connected to the database

---
+++ server.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const app = require("./app");
+const mongoose = require("mongoose");
+
+const DB = process.env.DATABASE.replace(
+  "<Password>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB).then(() => console.log("DB Connected successfully"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port ${process.env.PORT}...`);
+});
