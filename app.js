634e79d800c93383daa245e2e0fdb012740e32f7
Olaide Kolade <horlaide909@gmail.com>
2023-10-15 09:46:29 +0100

    chore : removed commented codes

--- app.js
+++ app.js
@@ -13,12 +13,6 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use("/api/v1/books", bookRouter);
 app.use("/api/v1/users", userRoute);
-// app.use((req, res, next) => {
-//   const error = new HttpError("Could not find this route.", 404);
-//   throw error;
-// });
-
-//app.use(ErrorHandler);
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,10 +6,6 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldsDB = (err) => {
-  // console.log(err);
-  //const value = err.keyValue.match(/(["'])(\\?.)*?\1/)[0];
-  //console.log(value);
-
   const message = `Duplicate ${Object.keys(err.keyValue).map(
     (el) => el
   )} value: ${
@@ -19,7 +15,6 @@ const handleDuplicateFieldsDB = (err) => {
 };
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
-  // console.log(errors);
   const message = `Invalid input data. ${errors.join(". ")}`;
   return new AppError(message, 400);
 };
@@ -61,11 +56,9 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    // let error = { ...err };
     if (err.name === "CastError") err = handleCastErrorDB(err);
     if (err.code === 11000) err = handleDuplicateFieldsDB(err);
     if (err.name === "ValidationError") err = handleValidationErrorDB(err);
-    //error = handleValidationErrorDB(error);
 
     sendErrorProd(err, res);
   }
