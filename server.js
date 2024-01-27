2fbc3761becefa2133effa5e4b42e31401714287
Olaide Kolade <horlaide909@gmail.com>
2024-01-27 15:41:44 +0100

    chore : keep service running instead of shutting down when there is an uncaught expression err

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
-process.on("uncaughtException", (err) => {
-  console.log(err.name, err.message);
-  console.log("UNCAUGHT EXCEPTION! Shutting down...");
-  process.exit(1);
-});
+
 const app = require("./app");
 const mongoose = require("mongoose");
 
@@ -14,14 +10,6 @@ const DB = process.env.DATABASE.replace(
 );
 mongoose.connect(DB).then(() => console.log("DB Connected successfully"));
 
-const server = app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}...`);
-});
-
-process.on("unhandledRejection", (err) => {
-  console.log(err.name, err.message);
-  console.log("UNHANDLED REJECTION! Shutting down...");
-  server.close(() => {
-    process.exit(1);
-  });
 });
