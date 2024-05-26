553b8e43741378f716f0ff673c5510684a7008e3
Olaide Kolade <horlaide909@gmail.com>
2024-05-26 07:46:09 +0100

    chore : create, update, get and delete a book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,8 +1,13 @@
 const Book = require("../models/bookModel");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
+const { validationResult } = require("express-validator");
 
 exports.addBook = catchAsync(async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   const newBook = await Book.create(req.body);
   res.status(201).json({
     status: "Success",
@@ -33,6 +38,15 @@ exports.getBook = catchAsync(async (req, res, next) => {
   });
 });
 exports.updateBook = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+  const findBook = await Book.findById(id);
+  if (!findBook) {
+    return next(new AppError("No book found with that ID", 404));
+  }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,12 +1,64 @@
 const express = require("express");
 const bookController = require("../controllers/bookController");
-
+const { body } = require("express-validator");
 const router = express.Router();
 
-router.post("/", bookController.addBook);
+router.post(
+  "/",
+  [
+    body("title").notEmpty().withMessage("A book must have a title"),
+    body("title")
+      .if(body("title").notEmpty())
+      .isLength({ min: 5 })
+      .withMessage("A book title must have more than 5 characters"),
+  ],
+  [body("genre").notEmpty().withMessage("A book must have a genre")],
+  [body("author").notEmpty().withMessage("A book must have an author")],
+  [
+    body("ISBN").notEmpty().withMessage("Please provide ISBN number"),
+    body("ISBN")
+      .if(body("ISBN").notEmpty())
+      .isLength({ min: 13, max: 13 })
+      .withMessage("ISBN number must be 13 in length"),
+  ],
+  [
+    body("overview").notEmpty().withMessage("A book must have an overview"),
+    body("overview")
+      .if(body("title").notEmpty())
+      .isLength({ min: 15 })
+      .withMessage("A book must not have an overview less than 15 characters"),
+  ],
+  bookController.addBook
+);
 router.get("/", bookController.getAllBooks);
 router.get("/:id", bookController.getBook);
-router.patch("/:id", bookController.updateBook);
+router.patch(
+  "/:id",
+  [
+    body("title").notEmpty().withMessage("A book must have a title"),
+    body("title")
+      .if(body("title").notEmpty())
+      .isLength({ min: 5 })
+      .withMessage("A book title must have more than 5 characters"),
+  ],
+  [body("genre").notEmpty().withMessage("A book must have a genre")],
+  [body("author").notEmpty().withMessage("A book must have an author")],
+  [
+    body("ISBN").notEmpty().withMessage("Please provide ISBN number"),
+    body("ISBN")
+      .if(body("ISBN").notEmpty())
+      .isLength({ min: 13, max: 13 })
+      .withMessage("ISBN number must be 13 in length"),
+  ],
+  [
+    body("overview").notEmpty().withMessage("A book must have an overview"),
+    body("overview")
+      .if(body("title").notEmpty())
+      .isLength({ min: 15 })
+      .withMessage("A book must not have an overview less than 15 characters"),
+  ],
+  bookController.updateBook
+);
 router.delete("/:id", bookController.deleteBook);
 
 module.exports = router;
