340a517fcd88a96ef42686dba7d71c500c911ac8
Olaide Kolade <horlaide909@gmail.com>
2024-06-08 19:50:37 +0100

    chore : return empty array when page requested for isn't found

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -40,11 +40,6 @@ exports.getAllBooks = catchAsync(async (req, res, next) => {
   const books = await query;
   const totalItems = await Book.countDocuments(JSON.parse(queryStr));
 
-  if (req.query.page) {
-    if (skip >= totalItems)
-      return next(new AppError("This page does not exist", 404));
-  }
-
   res.status(200).json({
     status: "Success",
     results: books.length,
@@ -73,11 +68,11 @@ exports.updateBook = catchAsync(async (req, res, next) => {
   const errors = validationResult(req);
   const id = req.params.id;
   const findBook = await Book.findById(id);
-  if (!findBook) {
-    return next(new AppError("No book found with that ID", 404));
-  }
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
+  }
+  if (!findBook) {
+    return next(new AppError("No book found with that ID", 404));
   }
   const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
